876d73400574e997b4b7fa626420a698347a1f89
Davca54 <274750@vutbr.cz>
2026-04-15 10:48:03 +0200

    vzory

--- searching.py
+++ searching.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import json
+import time
+
+import matplotlib.pyplot as plt
+from generators import unordered_sequence, ordered_sequence
 
 
 def read_data(file_name, field):
@@ -45,7 +49,36 @@ def linear_search(searched_data, searched_number):
     slovnik["count"] = count
     return slovnik
 
+def binary_search(searched_data, searched_number):
+    left = 0
+    right = len(searched_data) - 1
 
+    while left <= right:
+
+
+        mid = (left + right) // 2
+        prostredni_cislo = searched_data[mid]
+
+        if prostredni_cislo == searched_number:
+            return mid
+        elif prostredni_cislo < searched_number:
+            left = mid + 1
+        elif prostredni_cislo > searched_number:
+            right = mid - 1
+    return None
+
+
+
+def test_complexity(list_of_n):
+    number = 42
+    times_linear = []
+    times_binary = []
+    for n in list_of_n:
+        unordered_data = ordered_sequence(n)
+
+def pattern_search(sequenci, pattern):
+    indices = {}
+    return indices
 
 
 
@@ -54,11 +87,22 @@ def linear_search(searched_data, searched_number):
 
 
 def main():
-    my_data = read_data("sequential.json", "unordered_numbers")
+    my_data = read_data("sequential.json", "ordered_numbers")
     print(my_data)
 
     data = linear_search(my_data, 0)
     print(data)
+    duration = 0
+    rep = 100
+    for measurements in range(100):
+        start_time_linear = time.perf_counter()
+        found_num = linear_search(my_data, 0)
+        end_time_time_linear = time.perf_counter()
+        duration += end_time_time_linear - start_time_linear
+    print(duration)
+    ukol3 = binary_search(my_data, -1)
+    print(ukol3)
+
 
 if __name__ == "__main__":
     main()
